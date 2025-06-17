75e20e051370a717bc24f42b297f5e35dc90e318
Hans Würfel <git@wuerfel.io>
2025-06-17 18:57:44 +0200

    remove @async

--- src/MCPRepl.jl
+++ src/MCPRepl.jl
@@ -15,23 +15,22 @@ function execute_repllike(str)
     # expr = Meta.parse(str)
     expr = Base.parse_input_line(str)
     backend = repl.backendref
-    f = @async begin
-        sleep(3)
-        REPL.prepare_next(repl)
-        printstyled("\nagent> $str\n", color=:red, bold=:true)
-        response = REPL.eval_with_backend(expr, backend)
 
-        disp = IOBufferDisplay()
+    REPL.prepare_next(repl)
+    printstyled("\nagent> $str\n", color=:red, bold=:true)
+    response = REPL.eval_with_backend(expr, backend)
 
-        # generate printout, err goest to disp.err, val goes to "specialdisplay" disp
-        REPL.print_response(disp.io, response, backend, !REPL.ends_with_semicolon(str), false, disp)
+    disp = IOBufferDisplay()
 
-        # generate the printout again for the "normal" repl
-        REPL.print_response(repl, response, !REPL.ends_with_semicolon(str), repl.hascolor)
+    # generate printout, err goest to disp.err, val goes to "specialdisplay" disp
+    REPL.print_response(disp.io, response, backend, !REPL.ends_with_semicolon(str), false, disp)
 
-        REPL.prepare_next(repl)
-        REPL.LineEdit.refresh_line(repl.mistate)
-        String(take!(disp.io))
-    end
+    # generate the printout again for the "normal" repl
+    REPL.print_response(repl, response, !REPL.ends_with_semicolon(str), repl.hascolor)
+
+    REPL.prepare_next(repl)
+    REPL.LineEdit.refresh_line(repl.mistate)
+    String(take!(disp.io))
 end
+
 end # module MCPRepl
